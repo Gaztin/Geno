6a51ee4d27d81baf8bca36a1dcffb8b2584d6f53
Sebastian Kylander <developer@gaztin.com>
2021-07-20 11:38:07 +0200

    Make tablegen optional

--- premake5.lua
+++ premake5.lua
@@ -13,20 +13,23 @@ platforms( utils.get_platforms() )
 configurations { 'Debug', 'Release' }
 startproject( 'Geno' )
 
+if( _OPTIONS.tablegen ) then
+	third_party_library 'llvm-libclang-td'
+	third_party_library 'llvm-libllvm-ir-td'
+	third_party_library 'llvm-libllvm-omp-td'
+	third_party_library 'llvm-libllvm-target-td'
+	third_party_library 'llvm-clang-tablegen'
+	third_party_library 'llvm-tablegen'
+end
+
 third_party_library 'glew'
 third_party_library 'glfw'
 third_party_library 'imgui'
 third_party_library 'llvm-libclang'
-third_party_library 'llvm-libclang-td'
 third_party_library 'llvm-libllvm-ir'
-third_party_library 'llvm-libllvm-ir-td'
 third_party_library 'llvm-libllvm-omp'
-third_party_library 'llvm-libllvm-omp-td'
 third_party_library 'llvm-libllvm-target'
-third_party_library 'llvm-libllvm-target-td'
 third_party_library 'llvm-libllvm'
-third_party_library 'llvm-clang-tablegen'
-third_party_library 'llvm-tablegen'
 
 library 'Common'
 	sysincludedirs {
